22de4e8f406aa6a144d7f2f2c87f227f7edca5e2
PengDon <xxadress@sina.com>
2021-02-03 19:31:49 +0800

    样式调整

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "my-button",
+  name: 'my-button',
 
   props: {
     label: {
@@ -19,9 +19,9 @@ export default {
     },
     size: {
       type: String,
-      default: "medium",
-      validator: function (value) {
-        return ["small", "medium", "large"].indexOf(value) !== -1;
+      default: 'medium',
+      validator: function(value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1
       },
     },
     backgroundColor: {
@@ -32,27 +32,27 @@ export default {
   computed: {
     classes() {
       return {
-        "storybook-button": true,
-        "storybook-button--primary": this.primary,
-        "storybook-button--secondary": !this.primary,
+        'storybook-button': true,
+        'storybook-button--primary': this.primary,
+        'storybook-button--secondary': !this.primary,
         [`storybook-button--${this.size}`]: true,
-      };
+      }
     },
     style() {
       return {
         backgroundColor: this.backgroundColor,
-      };
+      }
     },
   },
 
   methods: {
     onClick() {
-      this.$emit("onClick");
+      this.$emit('onClick')
     },
   },
-};
+}
 </script>
-<style lang="css">
+<style lang="less">
 .storybook-button {
   font-weight: 700;
   border: 0;
@@ -60,26 +60,26 @@ export default {
   cursor: pointer;
   display: inline-block;
   line-height: 1;
-}
-.storybook-button--primary {
-  color: white;
-  background-color: #1ea7fd;
-}
-.storybook-button--secondary {
-  color: #333;
-  background-color: transparent;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-}
-.storybook-button--small {
-  font-size: 12px;
-  padding: 10px 16px;
-}
-.storybook-button--medium {
-  font-size: 14px;
-  padding: 11px 20px;
-}
-.storybook-button--large {
-  font-size: 16px;
-  padding: 12px 24px;
+  &-primary {
+    color: white;
+    background-color: #1ea7fd;
+  }
+  &-secondary {
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  }
+  &-small {
+    font-size: 12px;
+    padding: 10px 16px;
+  }
+  &-medium {
+    font-size: 14px;
+    padding: 11px 20px;
+  }
+  &-large {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
 }
 </style>
